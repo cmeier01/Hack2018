bcdbf8b6cea2fc2b0ec69a84d6d8009f2f9f9630
Caleb T. Dixon <cdixon05@vm-hw06.eecs.tufts.edu>
2018-10-12 23:56:10 -0400

    added options to button script

--- scripts/button.js
+++ scripts/button.js
@@ -7,9 +7,14 @@ var KEY = 47E95D014FC94726A766DE678392A41
 function getLocations(distance, type, lat, lng)
 {
 	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open( "GET","http://api.tripadvisor.com/api/partner/2.0/map/" + lat + "," + lng +"/" + type "?key=" + KEY, false );
+	xmlHttp.open( "GET","http://api.tripadvisor.com/api/partner/2.0/map/" + lat + "," + lng +"/" + type "?key=" + KEY + "?distance=" + distance, false );
 	xmlHttp.send( null );
 	return xmlHttp.responseText;
 }
 
 
+var str = getLocations(10, resturants, 42.418560, -71.106450);
+var file = new File("output.json");
+file.open("w"); // open file with write access
+file.write(str);
+file.close();
