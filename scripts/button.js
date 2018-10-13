de538889bd81505e9d792f5f073ee67483ea66d1
Caleb T. Dixon <cdixon05@vm-hw06.eecs.tufts.edu>
2018-10-13 00:53:44 -0400

    button script logs data to console

--- scripts/button.js
+++ scripts/button.js
@@ -1,12 +1,13 @@
 
 
 var KEY = "47E95D014FC94726A766DE678392A416";
-//type is exactly either "resturants" or "attractions"
+//type is exactly either "restaurants" or "attractions"
 //distance in miles user is willing to travel
 //lat and lng are latitude and longitude values of the user
 function getLocations(distance, type, lat, lng)
 {
 	var xmlHttp = new XMLHttpRequest();
+	//distance unused in this version
 	xmlHttp.open( "GET","http://api.tripadvisor.com/api/partner/2.0/map/" + lat + "," + lng + "/" + type + "?key=" + KEY, false );
 	xmlHttp.send();
 	return xmlHttp.responseText;
@@ -14,7 +15,4 @@ function getLocations(distance, type, lat, lng)
 
 
 var str = getLocations(10, "restaurants", "42.418560", "-71.106450");
-var file = new File("output.json");
-file.open("w"); // open file with write access
-file.write(str);
-file.close();
+console.log(str);
