ba5f207a18a717d655cb37197ec2b6d2e79914f6
Caleb T. Dixon <cdixon05@vm-hw06.eecs.tufts.edu>
2018-10-13 00:20:59 -0400

    continued debugging

--- scripts/button.js
+++ scripts/button.js
@@ -7,13 +7,13 @@ var KEY = "47E95D014FC94726A766DE678392A41";
 function getLocations(distance, type, lat, lng)
 {
 	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open( "GET","http://api.tripadvisor.com/api/partner/2.0/map/" + lat + "," + lng + "/" + type + "?key=" + KEY + "?distance=" + distance, false );
-	xmlHttp.send( null );
+	xmlHttp.open( "GET","http://api.tripadvisor.com/api/partner/2.0/map/" + lat + "," + lng + "/" + type + "?key=" + KEY, false );
+	xmlHttp.send();
 	return xmlHttp.responseText;
 }
 
 
-var str = getLocations(10, "resturants", 42.418560, -71.106450);
+var str = getLocations(10, "restaurants", "42.418560", "-71.106450");
 var file = new File("output.json");
 file.open("w"); // open file with write access
 file.write(str);
